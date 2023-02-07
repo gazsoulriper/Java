44e17da8db125c2b85b57e0f36274d85d1d70240
gazsoulriper <gazsoulriper@gmail.com>
2023-02-07 15:28:52 -0300

    sample program with intellij

---
+++ primeiro-programa-intellij/src/Main.java
@@ -0,0 +1,11 @@
+import Models.Gato;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*System.out.println("Hello world!");*/
+    }
+}
