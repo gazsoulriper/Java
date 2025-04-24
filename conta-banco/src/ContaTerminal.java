86f00118a5f8b02a25ac42097c24b6048d9ca773
gazsoulriper <gazsoulriper@gmail.com>
2025-04-23 23:27:09 -0300

    projeto para inserir dados de conta de banco simples finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Por favor, digite o número da Agência: ");
+            String agencia = scanner.next();
+
+            System.out.print("Por favor, digite o número da conta: ");
+            int numeroConta = scanner.nextInt();
+            scanner.nextLine();
+            
+            System.out.print("Por favor, digite o nome de usuário: ");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.print("Por favor, inserir o saldo da conta: ");
+            String saldoString = scanner.next();
+            BigDecimal saldo = new BigDecimal(saldoString);
+
+            scanner.close();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", o número da sua conta é " + numeroConta + " e seu saldo de R$ " + saldo + " já está disponível para saque!");
+
+
+        }
     }
 }
